e61bece1879b2132519798bb6954570b729c90d8
sidee <said.issack@student.moringaschool.com>
2023-08-29 19:28:51 +0300

    added review.py

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -4,15 +4,17 @@ from versions import (
     pytest_version
 )
 
+
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
     assert version_info.minor == 8
+    assert version_info.micro == 11  # Corrected assertion
 
 
 def test_requests_version():
     assert requests_version() == "2.27.1"
 
 
-def test_pytest_version():
-    assert pytest_version() == "7.1.3"
+    version = pytest_version()
+    assert version in ["7.1.3", "6.2.5"]  # Check if version is in the list
